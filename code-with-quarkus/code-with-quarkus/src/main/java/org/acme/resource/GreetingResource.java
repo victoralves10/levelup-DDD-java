4635308aee24a005d5dec54950e617bc58b871ed
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-14 20:05:01 -0300

    Add endpoint para listagem de enderecos
    Introduced a new GET endpoint '/listagem/enderecos' in GreetingResource to return a list of addresses using EnderecoService. Also refactored the existing people listing endpoint and updated path annotations for clarity.

--- code-with-quarkus/code-with-quarkus/src/main/java/org/acme/resource/GreetingResource.java
+++ code-with-quarkus/code-with-quarkus/src/main/java/org/acme/resource/GreetingResource.java
@@ -4,26 +4,37 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.acme.service.EnderecoService;
 import org.acme.service.PessoaService;
 
 import java.sql.SQLException;
 
-@Path("/loginPessoa")
+@Path("/listagem")
 public class GreetingResource {
 
     @Inject
     PessoaService pessoaService;
+    @Inject
+    EnderecoService enderecoService;
 
     @GET
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listar() {
+    public Response listarPessoas() {
         try{
             return Response.ok(pessoaService.listarGeral()).build();
         }catch (SQLException e){
             return Response.serverError().build();
         }
+    }
 
-
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/enderecos")
+    public Response listarEnderecos(){
+        try {
+            return Response.ok(enderecoService.listarEnderecos()).build();
+        } catch (SQLException e) {
+            return Response.serverError().build();
+        }
     }
 }
