f1f897e397a92e5d9185a142c84668b62fb2b6a6
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-14 18:06:20 -0300

    Add DTO_T_LOGIN class
    Introduced private fields 'login', 'senha', and 'st_ativo' to DTO_T_LOGIN. Added constructor, getters, and setters for these fields to support encapsulation and data transfer.

--- code-with-quarkus/code-with-quarkus/src/main/java/org/acme/model/DTO/DTO_T_LOGIN.java
+++ code-with-quarkus/code-with-quarkus/src/main/java/org/acme/model/DTO/DTO_T_LOGIN.java
@@ -1,4 +1,38 @@
 package org.acme.model.DTO;
 
 public class DTO_T_LOGIN {
+
+    private String login;
+    private String senha;
+    private String st_ativo;
+
+    public DTO_T_LOGIN(String login, String senha, String st_ativo) {
+        this.login = login;
+        this.senha = senha;
+        this.st_ativo = st_ativo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSt_ativo() {
+        return st_ativo;
+    }
+
+    public void setSt_ativo(String st_ativo) {
+        this.st_ativo = st_ativo;
+    }
 }
