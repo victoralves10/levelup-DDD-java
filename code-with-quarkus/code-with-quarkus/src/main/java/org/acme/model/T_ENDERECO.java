302c1bba5643bb638e94e4fd45d932ee1b60966e
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-14 17:39:22 -0300

    Add T_ENDERECO e seus campos

--- code-with-quarkus/code-with-quarkus/src/main/java/org/acme/model/T_ENDERECO.java
+++ code-with-quarkus/code-with-quarkus/src/main/java/org/acme/model/T_ENDERECO.java
@@ -1,4 +1,99 @@
 package org.acme.model;
 
 public class T_ENDERECO {
+    private long id_endereco;
+    private String cep;
+    private String pais;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private String complemento;
+
+    // Todos os campos
+    public T_ENDERECO(long id_endereco, String cep, String pais, String estado, String cidade, String bairro, String rua, String complemento) {
+        this.id_endereco = id_endereco;
+        this.cep = cep;
+        this.pais = pais;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.complemento = complemento;
+    }
+
+    // Construtor sem rua
+    public T_ENDERECO(long id_endereco, String cep, String pais, String estado, String cidade, String bairro, String rua) {
+        this.id_endereco = id_endereco;
+        this.cep = cep;
+        this.pais = pais;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+    }
+
+    public long getId_endereco() {
+        return id_endereco;
+    }
+
+    public void setId_endereco(long id_endereco) {
+        this.id_endereco = id_endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
