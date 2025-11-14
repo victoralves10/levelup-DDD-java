39e3fef6f199719b4b095b5e64c6404599057b7e
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-14 18:04:46 -0300

    Add T_LOGIN model
    Introduced a new T_LOGIN model to include fields for id_login, login, senha, and st_ativo, along with their getters, setters, and a full constructor.

---
+++ code-with-quarkus/code-with-quarkus/src/main/java/org/acme/model/DTO/DTO_T_LOGIN.java
@@ -0,0 +1,4 @@
+package org.acme.model.DTO;
+
+public class DTO_T_LOGIN {
+}
--- code-with-quarkus/code-with-quarkus/src/main/java/org/acme/model/T_LOGIN.java
+++ code-with-quarkus/code-with-quarkus/src/main/java/org/acme/model/T_LOGIN.java
@@ -1,4 +1,48 @@
 package org.acme.model;
 
 public class T_LOGIN {
+    private long id_login;
+    private String login;
+    private String senha;
+    private String st_ativo;
+
+    // CONSTRUTOR COMPLETO
+    public T_LOGIN(long id_login, String login, String senha, String st_ativo) {
+        this.id_login = id_login;
+        this.login = login;
+        this.senha = senha;
+        this.st_ativo = st_ativo;
+    }
+
+    public long getId_login() {
+        return id_login;
+    }
+
+    public void setId_login(long id_login) {
+        this.id_login = id_login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSt_ativo() {
+        return st_ativo;
+    }
+
+    public void setSt_ativo(String st_ativo) {
+        this.st_ativo = st_ativo;
+    }
 }
