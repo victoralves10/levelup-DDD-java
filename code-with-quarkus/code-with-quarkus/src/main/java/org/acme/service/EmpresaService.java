86becaf71a287c37b14d4eac49f510d5a9461e61
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-14 21:55:56 -0300

    Update EmpresaService.java

--- code-with-quarkus/code-with-quarkus/src/main/java/org/acme/service/EmpresaService.java
+++ code-with-quarkus/code-with-quarkus/src/main/java/org/acme/service/EmpresaService.java
@@ -1,6 +1,22 @@
 package org.acme.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.acme.model.T_EMPRESA;
+import org.acme.repository.T_EMPRESA_REPOSITORY;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@ApplicationScoped
 public class EmpresaService {
 
-    
+    @Inject
+    T_EMPRESA_REPOSITORY empresaRepository;
+
+    public List<T_EMPRESA> listarEmpresas () throws SQLException {
+
+        return empresaRepository.listarEmpresas();
+    }
+
 }
