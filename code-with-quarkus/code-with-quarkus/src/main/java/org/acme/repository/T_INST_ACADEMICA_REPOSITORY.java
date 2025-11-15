f444910b7054f8c1226fa0370fffb6facfe7ba7e
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-15 16:49:05 -0300

    Update T_INST_ACADEMICA_REPOSITORY.java

--- code-with-quarkus/code-with-quarkus/src/main/java/org/acme/repository/T_INST_ACADEMICA_REPOSITORY.java
+++ code-with-quarkus/code-with-quarkus/src/main/java/org/acme/repository/T_INST_ACADEMICA_REPOSITORY.java
@@ -25,8 +25,9 @@ public class T_INST_ACADEMICA_REPOSITORY {
 
             ResultSet rs = pst.executeQuery();
             while (rs.next()){
-                listaInstAcademica.add(new T_INST_ACADEMICA(rs.getLong(1),rs.getString(2),rs.getString(3),rs.getString(4)))
+                listaInstAcademica.add(new T_INST_ACADEMICA(rs.getLong(1),rs.getString(2),rs.getString(3),rs.getString(4)));
             }
+            return listaInstAcademica
         }
 
 
