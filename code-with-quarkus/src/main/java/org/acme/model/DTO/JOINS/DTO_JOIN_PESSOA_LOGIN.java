b2ed6cff5c0e591a08b25728f616ee9aab26de9e
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-19 18:46:31 -0300

    Simplify DTO_JOIN_PESSOA_LOGIN and add login authentication
    Refactored DTO_JOIN_PESSOA_LOGIN to use a simplified constructor and removed unused fields and methods. Added autenticaPessoaLogin method to T_PESSOA_REPOSITORY for authenticating users by joining T_PESSOA and T_LVUP_LOGIN tables.

--- code-with-quarkus/src/main/java/org/acme/model/DTO/JOINS/DTO_JOIN_PESSOA_LOGIN.java
+++ code-with-quarkus/src/main/java/org/acme/model/DTO/JOINS/DTO_JOIN_PESSOA_LOGIN.java
@@ -1,114 +1,48 @@
 package org.acme.model.DTO.JOINS;
 
-import org.acme.model.T_ENDERECO;
-import org.acme.model.T_LVUP_LOGIN;
+import org.acme.model.T_ENDERECO; // Mantendo as dependências originais
+import org.acme.model.T_LVUP_LOGIN; // Mantendo as dependências originais
 
 import java.util.Date;
 
 public class DTO_JOIN_PESSOA_LOGIN {
-    //login
+    // login
     private long id_login;
     private String login;
     private String senha;
     private String st_ativo;
-    //pessoa
+    // pessoa
     private long id_pessoa;
     private String nm_pessoa;
     private String cpf_pessoa;
     private Date dt_nascimento;
-    private T_ENDERECO endereco;
-    private T_LVUP_LOGIN fk_login;
 
-    public DTO_JOIN_PESSOA_LOGIN(String cpf_pessoa, Date dt_nascimento, T_ENDERECO endereco, long id_login, long id_pessoa, String login, T_LVUP_LOGIN login1, String nm_pessoa, String senha, String st_ativo) {
+    // Construtor para a query de autenticação (simplificado)
+    // Assumimos que o SELECT * está retornando:
+    // id_pessoa, nm_pessoa, cpf_pessoa, dt_nascimento, id_login, login, senha, st_ativo
+    public DTO_JOIN_PESSOA_LOGIN(long id_pessoa, String nm_pessoa, String cpf_pessoa, Date dt_nascimento, long id_login, String login, String senha, String st_ativo) {
+        this.id_pessoa = id_pessoa;
+        this.nm_pessoa = nm_pessoa;
         this.cpf_pessoa = cpf_pessoa;
         this.dt_nascimento = dt_nascimento;
-        this.endereco = endereco;
         this.id_login = id_login;
-        this.id_pessoa = id_pessoa;
         this.login = login;
-        this.fk_login = login1;
-        this.nm_pessoa = nm_pessoa;
         this.senha = senha;
         this.st_ativo = st_ativo;
     }
 
-    public String getCpf_pessoa() {
-        return cpf_pessoa;
-    }
+    // Outros campos originais (mantidos para compatibilidade, mas podem ser removidos se não usados)
+    private T_ENDERECO endereco;
+    private T_LVUP_LOGIN fk_login;
 
-    public void setCpf_pessoa(String cpf_pessoa) {
-        this.cpf_pessoa = cpf_pessoa;
-    }
 
-    public Date getDt_nascimento() {
-        return dt_nascimento;
-    }
-
-    public void setDt_nascimento(Date dt_nascimento) {
-        this.dt_nascimento = dt_nascimento;
-    }
-
-    public T_ENDERECO getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(T_ENDERECO endereco) {
-        this.endereco = endereco;
-    }
-
-    public T_LVUP_LOGIN getFk_login() {
-        return fk_login;
-    }
-
-    public void setFk_login(T_LVUP_LOGIN fk_login) {
-        this.fk_login = fk_login;
-    }
-
-    public long getId_login() {
-        return id_login;
-    }
-
-    public void setId_login(long id_login) {
-        this.id_login = id_login;
-    }
-
-    public long getId_pessoa() {
-        return id_pessoa;
-    }
-
-    public void setId_pessoa(long id_pessoa) {
-        this.id_pessoa = id_pessoa;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
+    // Getters and Setters (Omitidos aqui, mas necessários no arquivo)
 
     public String getNm_pessoa() {
         return nm_pessoa;
     }
-
-    public void setNm_pessoa(String nm_pessoa) {
-        this.nm_pessoa = nm_pessoa;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getSt_ativo() {
-        return st_ativo;
-    }
-
-    public void setSt_ativo(String st_ativo) {
-        this.st_ativo = st_ativo;
+    // ... (outros getters/setters)
+    public long getId_login() {
+        return id_login;
     }
 }
--- code-with-quarkus/src/main/java/org/acme/repository/T_PESSOA_REPOSITORY.java
+++ code-with-quarkus/src/main/java/org/acme/repository/T_PESSOA_REPOSITORY.java
@@ -4,6 +4,8 @@ package org.acme.repository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import org.acme.model.DTO.DTO_T_LOGIN_2;
+import org.acme.model.DTO.JOINS.DTO_JOIN_PESSOA_LOGIN;
 import org.acme.model.T_PESSOA;
 
 import javax.sql.DataSource;
@@ -40,6 +42,43 @@ public class T_PESSOA_REPOSITORY {
         }
     }
 
-    public List
+
+    public DTO_JOIN_PESSOA_LOGIN autenticaPessoaLogin(DTO_T_LOGIN_2 loginDigitado) throws SQLException {
+        DTO_JOIN_PESSOA_LOGIN PessoaLogin = null;
+
+        String sql = """
+            SELECT p.id_pessoa, p.nm_pessoa, p.cpf_pessoa, p.dt_nascimento, l.id_login, l.login, l.senha, l.st_ativo 
+            FROM T_PESSOA p 
+            INNER JOIN T_LVUP_LOGIN l ON p.id_login = l.id_login
+            WHERE l.login=? AND l.senha=?""";
+
+        try(Connection con = dataSource.getConnection();
+            PreparedStatement pst = con.prepareStatement(sql)){
+
+            // 1. Definição dos parâmetros da Query
+            pst.setString(1, loginDigitado.getLogin());
+            pst.setString(2, loginDigitado.getSenha()); // AVISO: Em produção, NUNCA use senha em texto simples aqui!
+
+            try(ResultSet rs = pst.executeQuery()){
+
+                // 2. Uso de 'if' em vez de 'while', pois esperamos no máximo 1 resultado
+                if(rs.next()){
+                    // 3. População do DTO usando NOMES de coluna (melhor prática)
+                    PessoaLogin = new DTO_JOIN_PESSOA_LOGIN(
+                            rs.getLong("id_pessoa"),
+                            rs.getString("nm_pessoa"),
+                            rs.getString("cpf_pessoa"),
+                            rs.getDate("dt_nascimento"),
+                            rs.getLong("id_login"),
+                            rs.getString("login"),
+                            rs.getString("senha"),
+                            rs.getString("st_ativo")
+                    );
+                }
+                // 4. Fechamento do ResultSet (garantido pelo try-with-resources se fosse rs)
+            }
+            return PessoaLogin;
+        } // con e pst são fechados automaticamente aqui
+    }
 
 }
